2f3125b628beb666e3050fbbc470ea30d147dd5b
JUNJIE LU <>
2019-11-01 11:02:24 +0800

    feat(init 163 source):

--- source/ntes.go
+++ source/ntes.go
@@ -1,5 +1,12 @@
 package source
 
+import (
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
 // Ntes 163
 type Ntes struct {
 }
@@ -10,20 +17,73 @@ func init() {
 
 // FetchLyric fetch lyric and save to local
 func (n *Ntes) FetchLyric(file string, artlist string, title string, duration int, size int) error {
+	pathIdx := strings.LastIndexAny(file, ".")
+	titleIdx := strings.LastIndexAny(file, "/")
+	dir := file[:titleIdx]
+	name := file[titleIdx+1 : pathIdx]
+
+	sid := n.FindSongID(name, "", "", duration, 0)
+
+	if sid > 0 {
+		lyrc, tlyrc, err := n.DownloadLyric(sid)
+		if err != nil {
+			return err
+		}
+
+		if len(lyrc) > 0 {
+			path := dir + "/" + name + ".lyric"
+			err := save(path, strings.NewReader(lyrc))
+			if err != nil {
+				return err
+			}
+		}
+
+		if len(tlyrc) > 0 {
+			path := dir + "/" + name + ".t.lyric"
+			err := save(path, strings.NewReader(tlyrc))
+			if err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
 
 // FindSongID find song id
-func (n *Ntes) FindSongID(file string, artlist string, title string, duration int, size int) int {
+// TODO:
+func (n *Ntes) FindSongID(name string, artlist string, title string, duration int, size int) int {
 	return 0
 }
 
 // DownloadLyric download ly
-func (n *Ntes) DownloadLyric(id int) error {
-	return nil
+// TODO:
+func (n *Ntes) DownloadLyric(id int) (string, string, error) {
+	return "", "", nil
 }
 
 // NewNtes new
 func NewNtes() LyricSource {
 	return &Ntes{}
+}
+
+func getLyric(id int) (string, string) {
+	return "", ""
+}
+
+func save(path string, src io.Reader) error {
+	out, err := os.Create(path)
+	defer out.Close()
+	if err != nil {
+		log.Printf("Write eror: %v \n", err)
+		return err
+	}
+	n, err := io.Copy(out, src)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	log.Printf("<- %v, size: %v\n", path, n)
+
+	return err
 }
--- source/source.go
+++ source/source.go
@@ -3,8 +3,9 @@ package source
 // LyricSource source
 type LyricSource interface {
 	FetchLyric(file string, artlist string, title string, duration int, size int) error
-	FindSongID(file string, artlist string, title string, duration int, size int) int
-	DownloadLyric(id int) error
+	FindSongID(name string, artlist string, title string, duration int, size int) int
+	DownloadLyric(id int) (string, string, error)
 }
 
+// LyricSrc lyric source
 var LyricSrc = make(map[string]LyricSource)
